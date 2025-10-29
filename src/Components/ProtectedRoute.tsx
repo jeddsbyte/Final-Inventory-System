74eace77f91d4b47b699938f945cd028a5a06d6c
Ngaburiaa <dennismathenge035@gmail.com>
2025-10-29 17:03:56 +0300

    jedds

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import type { RootState } from '../App/store';
 
-
 type ProtectedRouteProps = {
     children: React.ReactNode;
 };
